de808940a8a1a0dc47bbba8f6c19d8d3b7bdd317
Jean AIH <jeanaih016@gmail.com>
2026-02-21 18:31:51 +0800

    Initial commit

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ MESSAGE=${1:-"Update"}
 git config user.name "Jeanaih"
 git config user.email "jeanaih016@gmail.com"
 if ! git remote | grep -q origin; then
-    git remote add origin https://github.com/jeanaih/cook.git
+    git remote add origin git@github.com:jeanaih/cook.git
 fi
 git add .
 if git diff --quiet --cached; then
