180c8eaaff1b624e7b2b74ebd54af0cf89192800
Jean AIH <jeanaih016@gmail.com>
2026-02-21 18:25:00 +0800

    Initial commit

--- push.sh
+++ push.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 git config user.name "Jeanaih"
 git config user.email "jeanaih016@gmail.com"
-git init
 git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/jeanaih/cook.git
-git push -u origin main
+git commit -m "Update"
+git push origin main
